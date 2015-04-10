961d04761ccebc51679afcdc68a56a7b1cc34f1a
Daniel <daniel.alvarez.vaquero@gmail.com>
2015-04-10 12:35:06 +0200

    Añadida la posibilidad de moverse en direccion noroeste

--- Room.java
+++ Room.java
@@ -20,6 +20,7 @@ public class Room
     private Room eastExit;
     private Room westExit;
     private Room southEastExit;
+    private Room northWestExit;
     /**
      * Create a room described "description". Initially, it has
      * no exits. "description" is something like "a kitchen" or
@@ -39,7 +40,7 @@ public class Room
      * @param south The south exit.
      * @param west The west exit.
      */
-    public void setExits(Room north, Room east, Room south, Room west, Room southEast) 
+    public void setExits(Room north, Room east, Room south, Room west, Room southEast, Room northWest) 
     {
         if(north != null)
             northExit = north;
@@ -51,6 +52,8 @@ public class Room
             westExit = west;
         if(southEast != null)
             southEastExit = southEast;
+        if(northWest != null)
+            northWestExit = northWest;
     }
 
     /**
@@ -75,6 +78,8 @@ public class Room
             exit = westExit; break;
             case "southEast":
             exit = southEastExit; break;
+            case "northWest":
+            exit = northWestExit; break;
             default:
             exit = null;
         }
